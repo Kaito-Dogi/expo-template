63d78c8c4a85da538ff547df4b3f7e5144ea95b9
Kaito-Dogi <kendobu0405@gmail.com>
2023-11-07 00:17:10 +0900

    refactor: HomeScreen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import { FC } from "react";
 import { SafeAreaView, Text } from "react-native";
 
-import { styles } from "./style";
+import { styles } from "./styles";
 
 type Props = {
   text: string;
@@ -15,7 +14,6 @@ export const HomeScreen: FC<Props> = ({ text }) => {
   return (
     <SafeAreaView style={styles.container}>
       <Text>{text}</Text>
-      <StatusBar style="auto" />
     </SafeAreaView>
   );
 };
